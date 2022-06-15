1684eb0debb3f1e17fb214ee1d573eb0980625a9
Joel Tanner Jones <jonesjoe@ecu.edu>
2022-06-15 16:29:01 -0400

    Adds application class

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "tic_tac_toe/version"
+require_relative "tic_tac_toe/application"
 require_relative "tic_tac_toe/board"
 
 module TicTacToe
   class Error < StandardError; end
-  # Your code goes here...
 end
---
+++ lib/tic_tac_toe/application.rb
@@ -0,0 +1,3 @@
+module TicTacToe
+  class Application; end
+end
