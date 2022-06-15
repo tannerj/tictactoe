e9d4ecbf30e912f29d91e88c93e5c84639220b3c
Joel Tanner Jones <jonesjoe@ecu.edu>
2022-06-15 16:59:35 -0400

    Adds TicTacToe::Board.update_state
    TicTacToe:Board.update_state controls the state of the board.

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -1,8 +1,40 @@
+require 'io/console'
+
 module TicTacToe
 class Board
+  attr_reader :state
+
+  def initialize
+    @state = {
+      A1: " ",
+      A2: " ",
+      A3: " ",
+      B1: " ",
+      B2: " ",
+      B3: " ",
+      C1: " ",
+      C2: " ",
+      C3: " "
+    }
+  end
 
   def render
-    puts "test"
+    puts "   1   2   3 "
+    puts "A  #{state[:A1]} | #{state[:A2]} | #{state[:A3]} "
+    puts "  ---+---+---"
+    puts "B  #{state[:B1]} | #{state[:B2]} | #{state[:B3]} "
+    puts "  ---+---+---"
+    puts "C  #{state[:C1]} | #{state[:C2]} | #{state[:C3]} "
+  end
+
+def update_state(cell:, value:)
+  raise TicTacToe::BoardError.new("Cell out of bounds.") if !@state.has_key? cell.to_sym
+  raise TicTacToe::BoardError.new("Unauthorized value.") unless value.to_s.upcase == "X" || value.to_s.upcase == "O"
+  @state[cell.to_sym] = value.upcase
+end
+
+  def clear
+    $stdout.clear_screen
   end
 end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,28 @@
 require 'spec_helper'
 
 RSpec.describe TicTacToe::Board do
+
   it "renders tic tac toe board" do
-    board = TicTacToe::Board.new
-    expect { board.render() }.to output("test\n").to_stdout
+    expect { subject.render() }.to output("   1   2   3 \nA    |   |   \n  ---+---+---\nB    |   |   \n  ---+---+---\nC    |   |   \n").to_stdout
+
+  end
+
+  describe "Board::update_state" do
+    it "udpates the state" do
+      subject.update_state( cell: "A1", value: "X" )
+      expect(subject.state[:A1]).to eq("X")
+    end
+
+    it "rejects out of bounds cells" do
+      expect { subject.update_state( cell: "A5", value: "X" ) }.to(
+        raise_error(TicTacToe::BoardError, "Cell out of bounds.") 
+      )
+    end
+
+    it "rejects unauthorized values" do
+      expect { subject.update_state( cell: "A1", value: "Z" ) }.to(
+        raise_error(TicTacToe::BoardError, "Unauthorized value.")
+      )
+    end
   end
 end
