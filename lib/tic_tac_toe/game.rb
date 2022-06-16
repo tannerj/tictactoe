b1da6654a309959be9555a859fc63e1b62de0027
Joel Tanner Jones <jonesjoe@ecu.edu>
2022-06-16 09:23:05 -0400

    Adds TicTacToe::Game#update_game_state
    Adds a 2D array of solution cells to check for winning the game and
    updates TicTacToe::Game#game_state which controls the flow of the game.

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -1,17 +1,36 @@
 module TicTacToe
   class Game
-    attr_reader :current_player, :turn_stack
+    attr_reader :current_player, :turn_stack, :game_state
+    
+    GAME_STATE_OPEN   = "open"
+    GAME_STATE_CLOSED = "closed"
+    SOLUTION_SETS = [
+      [:A1, :A2, :A3],
+      [:B1, :B2, :B3],
+      [:C1, :C2, :C3],
+      [:A1, :B1, :C1],
+      [:A2, :B2, :C2],
+      [:A3, :B3, :C3],
+      [:A1, :B2, :C3],
+      [:A3, :B2, :C1]
+    ]
 
-    def initialize(args)
+    def initialize(args={})
+      initial_player = set_initial_player()
       @board = args.fetch(:board, TicTacToe::Board.new)
       @turn_stack = args.fetch(:turn_stack, [])
-      @current_player = args.fetch(:current_player, set_initial_player())
+      @current_player = args.fetch(:current_player, initial_player)
+      @game_state = GAME_STATE_OPEN
     end
 
     def player_turn(cell:)
-      @turn_stack.push([@current_player, cell])
-      @board.update_state( cell: cell, value: @current_player )
-      @current_player = get_next_player()
+      if @game_state == GAME_STATE_OPEN
+        @turn_stack.push([@current_player, cell])
+        @board.update_state( cell: cell, value: @current_player )
+        update_game_state()
+      end
+
+      @current_player = get_next_player() 
     end
 
     private 
@@ -20,7 +39,18 @@ module TicTacToe
     end
 
     def get_next_player
-      @current_player == "X" ? "O" : "X"
+      if @game_state == GAME_STATE_OPEN
+        @current_player == "X" ? "O" : "X"
+      end
+    end
+
+    def update_game_state()
+      win_string = @current_player * 3
+      SOLUTION_SETS.each do |set|
+        solution_state = ""
+        solution_state << @board.state[set[0]] << @board.state[set[1]] << @board.state[set[2]]
+        @game_state = GAME_STATE_CLOSED if solution_state = win_string
+      end
     end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe TicTacToe::Game do
-  
-  subject { TicTacToe::Game.new(board: TicTacToe::Board.new) }
 
+  it "sets game_state to open" do
+    expect(subject.game_state).to eq(TicTacToe::Game::GAME_STATE_OPEN)
+  end
+  
   it "Adds a player turn to turn_stack" do
     current_player = subject.current_player
     expect{ subject.player_turn(cell: "A1") }.to change { subject.turn_stack.count }.by(1)
@@ -11,10 +13,27 @@ RSpec.describe TicTacToe::Game do
   end
 
   it "updates the current_player after each player_turn" do
-    subject { TicTacToe::Game.new(board: TicTacToe::Board.new, current_player: "X") }
+    current_player = subject.current_player
     subject.player_turn(cell: "B2")
-    expect(subject.current_player).to eq("O")
-    subject.player_turn(cell: "A1")
-    expect(subject.current_player).to eq("X")
+    expect(subject.current_player).to_not eq(current_player)
+  end
+
+  it "calls Board#update_state" do
+    board = double()
+    allow(board).to receive(:state).and_return(TicTacToe::Board.new.state)
+    game = TicTacToe::Game.new( board: board )
+    expect(board).to receive(:update_state).with(cell: "A1", value: game.current_player)
+
+    game.player_turn( cell: "A1" )
+  end
+
+  it "detects a winning move" do
+    board = TicTacToe::Board.new
+    board.update_state(cell: "A1", value: "X")
+    board.update_state(cell: "A2", value: "X")
+    game = TicTacToe::Game.new( board: board, current_player: "X" )
+    game.player_turn( cell: "A3" )
+
+    expect(game.game_state).to eq(TicTacToe::Game::GAME_STATE_CLOSED)
   end
 end
